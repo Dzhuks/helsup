3f428d628f32cdd9a0e8d369db14ed27e708c01e
Seidazymov Adil <m.kypshakb@gmail.com>
2023-03-28 12:04:54 +0300

    static заработало!Маха и чел их тг харош!Адиль гей

--- helsup/helsup/settings.py
+++ helsup/helsup/settings.py
@@ -127,10 +127,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
-STATICFILES_DIRES = (
+STATIC_URL = '/static/'
+STATICFILES_DIRS = (
     BASE_DIR / 'static',
 )
+
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'

c5ef0337bc0c8a31e337ead66b15a32b7098afa0
Seidazymov Adil <m.kypshakb@gmail.com>
2023-04-15 22:23:47 +0300

    is_instance -> isinstance in users_extras

--- helsup/users/templatetags/users_extras.py
+++ helsup/users/templatetags/users_extras.py
@@ -4,6 +4,6 @@ register = template.Library()
 
 
 @register.filter
-def is_instance(user, model_name):
+def isinstance(user, model_name):
     model = getattr(user, '__class__')
     return model.__name__.lower() == model_name.lower()
